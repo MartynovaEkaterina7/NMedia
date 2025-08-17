9a2e5e42895be715e4b109711ec669591474e984
Мартынова Екатерина Михайловна <ekaterina.martynova89@mail.ru>
2025-08-18 02:09:57 +0300

    Домашнее задание к занятию «Обработка событий в Android»

---
+++ app/src/main/java/ru/netology/nmedia/dto/Post.kt
@@ -0,0 +1,28 @@
+package ru.netology.nmedia.dto
+
+import android.icu.text.DecimalFormat
+
+data class Post(
+    val id: Int,
+    val author: String,
+    val published: String,
+    val content: String,
+    var countLikes: Int = 999,
+    var likeByMe: Boolean = false,
+    var countReposts: Int = 302
+)
+
+object WallService {
+
+    fun textForCounts(count: Int): String {
+        val dfRound = DecimalFormat("#")
+        val dfNoRound = DecimalFormat("#.#")
+
+        return when {
+            count > 999 && count < 10_000 -> "${dfNoRound.format(count / 1_000.0)}K"
+            count >= 10_000 && count < 1_000_000 -> "${dfRound.format(count / 1_000.0)}K"
+            count >= 1_000_000 -> "${dfNoRound.format(count / 1_000_000.0)}M"
+            else -> count.toString()
+        }
+    }
+}
